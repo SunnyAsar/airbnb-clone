29b3aa3b70ae4bc258f0ff531c44029013829598
Sunny Asar <asarsunny@gmail.com>
2020-10-01 17:46:20 +0100

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'reac'
+
+
 import Home from './componenets/Home'
 import Header from './componenets/Header'
 import Footer from './componenets/Footer'
